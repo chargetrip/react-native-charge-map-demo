33dbd21652d43afe8e193ce0ec29ab03a7315587
Tim Kuilman <tim.kuilman@gmail.com>
2020-10-13 10:59:13 +0200

    feat: initialise headers on the native iOS side

--- ios/ReactNativeStationMap/AppDelegate.m
+++ ios/ReactNativeStationMap/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "MGLCustomHeaders.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -31,6 +32,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  [[MGLCustomHeaders sharedInstance] initHeaders];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"ReactNativeStationMap"
